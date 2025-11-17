abfddae549facb14ba6fe45ff7847344f821b4bf
TanBinh <buitanbinh2005@gmail.com>
2025-11-17 14:28:00 +0700

    cap nhat lại 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import routeter from "./routers/router";
 import { Suspense } from "react";
-import { SidebarProvider } from "@/contexts/SidebarProvider.jsx";
+import { SidebarProvider } from "@/contexts/SideBarProvider.jsx";
 import SideBar from "@components/SideBar/SideBar.jsx";
 import { ToastProvider } from "@/contexts/ToastProvider.jsx";
 import { StoreProvider } from "@/contexts/StoreProvider.jsx";
